4cc720ef64c11ec2b8bbdb050f33105d9f5db476
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-08-17 16:28:11 +0300

    push all

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "modern-normalize";
 import "./index.css";
-import App from ".App.jsx";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
